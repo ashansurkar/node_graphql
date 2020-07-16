670d26751913f35a8ab3c448fad0be8d72ad7a6f
ashansurkar <59321102+ashansurkar@users.noreply.github.com>
2020-07-16 16:28:17 +1200

    mutation create done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { GraphQLServer } from 'graphql-yoga';
+import { v4 as uuidv4 } from 'uuid';
 
 const users = [{
     id : '1',
@@ -42,23 +43,28 @@ const posts = [{
 
 const comments = [{
     id : '1',
-    text : 'comment 1',
-    author: '1'
+    text : 'comment for node',
+    author: '1',
+    post : '3'
 },
 {
     id : '2',
-    text : 'comment 2',
-    author: '2'
+    text : 'comment 2 for node',
+    author: '2',
+    post : '3'
 },
 {
     id : '3',
-    text : 'comment 3',
-    author: '2'
+    text : 'comment for JS',
+    author: '2',
+    post : '2'
 },
 {
     id : '4',
-    text : 'comment 4',
-    author: '3'
+    text : 'comment for graphql',
+    author: '3',
+    post: '1'
+
 }]
 //type definition
 const typeDefs = `
@@ -67,6 +73,27 @@ const typeDefs = `
         me : User!
         posts : [Post!]!
         comments : [Comment!]!
+    }
+    type Mutation{
+        createUser(data : CreateUserInput!): User!
+        createPost(data : CreatePostInput!): Post!
+        createComment(data : CreateCommentInput!): Comment!
+    }
+    input CreateUserInput{
+        name:String!
+        email:String!
+        age: Int
+    }
+    input CreatePostInput{
+        title: String!
+        body:String!
+        published: Boolean!
+        author: ID!
+    }
+    input CreateCommentInput{
+        text:String!
+        author:ID!
+        post:ID!
     }
     type User {
         id : ID!
@@ -82,11 +109,13 @@ const typeDefs = `
         body:String!
         published:Boolean!
         author : User!
+        comments : [Comment!]!
     }
     type Comment{
         id:ID!
         text:String!
         author : User!
+        post: Post!
     }
     
 `;
@@ -113,7 +142,6 @@ const resolvers = {
             if(!args.query){
                 return comments
             }
-
         },
         users(parent,args,ctx,info){
             if(!args.query){
@@ -122,9 +150,54 @@ const resolvers = {
             return users.filter(user=> user.name.toLowerCase().includes(args.query.toLowerCase()))
         }
     },
+    Mutation:{
+        createUser(parent,args,ctx,info){
+            const emailTaken = users.some( user=> user.email === args.data.email );
+            if(emailTaken){
+                throw new Error('Email has been taken');
+            }
+            const user = {
+                id : uuidv4(),
+                ...args.data
+            }
+            users.push(user);
+            return user
+        },
+        createPost(parent,args,ctx,info){
+            const user = users.some( user=> user.id === args.data.author );
+            if(!user){
+                throw new Error('Author not found')
+            }
+            const post = {
+                id : uuidv4(),
+                ...args.data
+            }
+            posts.push(post);
+            return post;
+        },
+        createComment(parent,args,ctx,info){
+            const user = users.some( user=> user.id === args.data.author );
+            const post = posts.some( post=> post.id === args.data.post && post.published);
+            if(!user){
+                throw new Error('Author not found')
+            }
+            if(!post){
+                throw new Error('Post not found')
+            }
+            const comment = {
+                id : uuidv4(),
+                ...args.data
+            };
+            comments.push(comment);
+            return comment;
+        }
+    },
     Post:{
         author(parent,args,ctx,info){
             return users.find( user => parent.author === user.id);
+        },
+        comments(parent,args,ctx,info){
+            return comments.filter( comm => comm.post === parent.id);
         }
     },
     User: {
@@ -138,8 +211,11 @@ const resolvers = {
     Comment:{
         author(parent,args,ctx,info){
             return users.find( user => parent.author === user.id);
+        },
+        post(parent,args,ctx,info){
+            return posts.find( post => parent.post === post.id);
         }
-    }
+    } 
 }
 
 const server = new GraphQLServer({typeDefs,resolvers});
